6011980460a47a8925af977380192a7134296d93
Ahmed Mahrous <ahmed.mahrous@gomini-app.com>
2023-08-18 15:01:07 +0300

    Close Add Network Layer #2

---
+++ Currency/Network/SSLPinningSessionManager.swift
@@ -0,0 +1,34 @@
+//
+//  SSLPinningSessionManager.swift
+//  Currency
+//
+//  Created by Ahmed Mahrous on 17/08/2023.
+//
+
+//MARK: - You have to make pod install to get Moya, Alamofire and reopen the project
+import Moya
+import Alamofire
+
+
+final class SSLPinningSessionManager: Session {
+    
+    static let shared: Session = {
+        let configuration = URLSessionConfiguration.default
+        configuration.timeoutIntervalForRequest = 10
+        configuration.timeoutIntervalForResource = 10
+        
+        // Load your pinned public key data
+        guard let pinnedCertificatePath = Bundle.main.path(forResource: "sni.cloudflaressl.com", ofType: "cer"),
+              let pinnedCertificateData = try? Data(contentsOf: URL(fileURLWithPath: pinnedCertificatePath)) else {
+            fatalError("Pinned certificate data not found")
+        }
+        
+        let serverTrustManager = ServerTrustManager(evaluators: [
+            "api.apilayer.com": PinnedCertificatesTrustEvaluator(certificates: [SecCertificateCreateWithData(nil, pinnedCertificateData as CFData)!])
+        ])
+        
+        let session = Session(configuration: configuration, serverTrustManager: serverTrustManager)
+        
+        return session
+    }()
+}
